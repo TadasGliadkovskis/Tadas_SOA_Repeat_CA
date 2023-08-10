9b1a20a48fc6e0179f9b8535a9dff7259f040a2f
Ted <tadas2410@gmail.com>
2023-08-10 03:24:45 +0100

    basic Get All request

---
+++ Controllers/GameController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tadas_SOA_Repeat_CA.Models;
+
+namespace Tadas_SOA_Repeat_CA.Controllers
+{
+    [Route("api/Game")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Game> GetGames()
+        {
+            return new List<Game>
+            {
+                new Game{Id=1, Name="Doom"},
+                new Game{Id=2, Name="Animal Crossing"}
+            };
+        }
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,8 @@
+﻿namespace Tadas_SOA_Repeat_CA.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
